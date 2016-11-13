148d7cf3c6d1593ebe96aefeb61141e02334707e
Bdmms <mmsrannie@gmail.com>
2016-11-12 22:22:50 -0500

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -1,16 +1,11 @@
-import java.awt.Rectangle;
-/*
-import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiEvent;
-import javax.sound.midi.MidiMessage;
-import javax.sound.midi.ShortMessage;
-*/
 
 /**
  * Date: October 26, 2016
  * 
  * This class contains all of the data effecting the notes
- * inside the song
+ * inside the song. Notes are only loaded when entering the
+ * note editor of a track. Only one track's notes are loaded
+ * at a time.
  */
 
 public class Notes
@@ -21,28 +16,40 @@ public class Notes
 	public final static byte DATA_VELOCITY = 2;	//value assigned to the volume of the note
 	
 	private static int numNotes = 0;	//Number of notes processed
+	private static byte track = 0;		//Track being accessed
 	
 	private int begin = 0;				//start of note in sequence
 	private int end = 0;				//end of note in sequence
 	private byte tone = 60;				//tone of the note
+	private byte volume = 0;			//volume of the note
 	
-	//Initial method
+	//Constructor method
 	//int s = start of note (in array)
 	//int e = end of note (in array)
 	public Notes(int s, int e)
 	{
 		begin = s;
 		end = e;
-		tone = MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(s).getMessage().getMessage()[DATA_TONE];
+		tone = MIDISong.getMessage(MIDIMain.getTrackMenu(), s).getMessage()[DATA_TONE];
+		volume =  MIDISong.getMessage(MIDIMain.getTrackMenu(), s).getMessage()[DATA_VELOCITY];
 		numNotes++;
 	}
 	
-	//contains(int x, int y) checks whether the sent coordinates are inside the note's rectangle and returns true or false
-	//int x = x coordinate of mouse
-	//int y = y coordinate of mouse
+	//contains(long x, short y) checks whether the sent coordinates are inside the note's rectangle and returns true or false
+	//long x = x coordinate of mouse
+	//short y = y coordinate of mouse
 	public boolean contains(long x, short y)
 	{
-		return new Rectangle((int)getX(), getY(), getLength(), MIDIMain.getPreHeight()).contains((int)x, y);
+		//If x coordinate is between the x values of the note
+		if(x >= getX() && x <= getX() + getLength())
+		{
+			//If y coordinate is between the y values of the note
+			if(y >= getY() && y <= getY() + MIDIMain.getPreHeight())
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	//identifyContained(int x, int y) checks every existing note to determine which note is being clicked on, the object number of the note is returned
@@ -50,13 +57,14 @@ public class Notes
 	//int y = y coordinate of mouse
 	public static int identifyContained(long x, short y)
 	{
+		//Notes are not sorted
 		for(int i = 0; i < numNotes; i++)
 		{
 			//If note is on screen
 			if(GUI.isNoteVisible(i))
 			{
 				//If note contains coordinates
-				if(MIDISong.getNotes(MIDIMain.getTrackMenu())[i].contains(x, y))
+				if(MIDISong.getNotes(track)[i].contains(x, y))
 				{
 					return i;
 				}
@@ -69,70 +77,7 @@ public class Notes
 	//byte t = new tone
 	public void setTone(byte t)
 	{
-		/*
-		if(t != MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getMessage().getMessage()[DATA_TONE])
-		{
-			try {
-				MidiEvent eve = new MidiEvent(new ShortMessage(ShortMessage.NOTE_ON, MIDIMain.getTrackMenu(), t, MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getMessage().getMessage()[DATA_VELOCITY]), MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getTick());
-				for(int i = 0; i < begin; i++)
-				{
-					if(MIDISong.getNotes(MIDIMain.getTrackMenu())[i].getEnd() == begin && MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(i).getMessage().getStatus() == ShortMessage.NOTE_ON)
-					{
-						MidiEvent eveEnd = new MidiEvent(new ShortMessage(ShortMessage.NOTE_OFF, MIDIMain.getTrackMenu(), MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getMessage().getMessage()[DATA_TONE], MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getMessage().getMessage()[DATA_VELOCITY]), MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(i).getTick());
-						MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].remove(MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(i));
-						MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].add(eveEnd);
-					}
-					if(i == begin - 1)
-					{
-						MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].remove(MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin));
-					}
-				}
-				MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].add(eve);
-				
-				for(int i = 0; i < MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].size(); i++)
-				{
-					if(MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(i).equals(eve))
-					{
-						begin = i;
-						break;
-					}
-				}
-		
-				MidiEvent eve2 = new MidiEvent(new ShortMessage(ShortMessage.NOTE_OFF, MIDIMain.getTrackMenu(), t, MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(end).getMessage().getMessage()[DATA_VELOCITY]), MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(end).getTick());
-				for(int i = end; i < numNotes; i++)
-				{
-					if(MIDISong.getNotes(MIDIMain.getTrackMenu())[i].getBeginning() == end && MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(end).getMessage().getStatus() == ShortMessage.NOTE_ON)
-					{
-						MidiEvent eveStart = new MidiEvent(new ShortMessage(ShortMessage.NOTE_ON, MIDIMain.getTrackMenu(), MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getMessage().getMessage()[DATA_TONE], MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getMessage().getMessage()[DATA_VELOCITY]), MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(i).getTick());
-						MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].remove(MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(i));
-						MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].add(eveStart);
-					}
-					if(i == numNotes - 1)
-					{
-						MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].remove(MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(end));
-					}
-				}
-				MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].add(eve2);
-				
-				for(int i = 0; i < MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].size(); i++)
-				{
-					if(MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(i).equals(eve2))
-					{
-						end = i;
-						break;
-					}
-				}
-			} catch (ArrayIndexOutOfBoundsException e) {} catch (InvalidMidiDataException e) {}
-		}
-		*/
 		tone = t;
-	}
-	
-	//setLength(int l) sets the length of the note
-	//int l = length of note
-	public void setLength(int l)
-	{
-		setEnd(begin + l);
 	}
 	
 	//setEnd(long x) sets the end location of the note
@@ -141,15 +86,15 @@ public class Notes
 	{
 		x = (x - x%MIDIMain.getPreLength()) / MIDIMain.getPreLength();
 			
-		if(x <= 0 || x <= getX())
-			x = getX() + 1;
+		if(x <= 0 || x <= getTick())
+			x = getTick() / MIDIMain.getPreLength() + 1;
 		
-		MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(end).setTick(x);
+		MIDISong.getEvent(track, end).setTick(x);
 	}
 	
 	//setLocation(byte t) sets the location of the note
-	//int x = x location of note
-	//int y = tone / y location of note
+	//long x = x location of note
+	//short y = tone / y location of note
 	public void setLocation(long x, short y)
 	{
 		x = (x - x%MIDIMain.getPreLength()) / MIDIMain.getPreLength();
@@ -160,9 +105,16 @@ public class Notes
 			y = 0;
 		if(y > 120)
 			y = 120;
-		MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(end).setTick(x + getLength()/MIDIMain.getPreLength());
-		MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).setTick(x);
+		MIDISong.getEvent(track, end).setTick(x + getLength()/MIDIMain.getPreLength());
+		MIDISong.getEvent(track, begin).setTick(x);
 		setTone((byte) y);
+	}
+	
+	//setTrack(byte trackNum) sets the track that notes come from
+	//byte trackNum = track in sequence
+	public static void setTrack(byte trackNum)
+	{
+		track = trackNum;
 	}
 	
 	//getBeginning()() returns the point of the star message in the array of messages
@@ -177,10 +129,16 @@ public class Notes
 		return end;
 	}
 	
+	//getTick() returns the tick of the note
+	public long getTick()
+	{
+		return MIDISong.getEvent(track, begin).getTick();
+	}
+	
 	//getX() returns the x location of the note
 	public long getX()
 	{
-		return MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getTick() * MIDIMain.getPreLength();
+		return MIDISong.getEvent(track, begin).getTick() * MIDIMain.getPreLength();
 	}
 	
 	//getY() returns the y location of the note
@@ -199,13 +157,14 @@ public class Notes
 	//getLength() returns the exact length of the note
 	public int getLength()
 	{
-		return (int)((MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(end).getTick() - MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getTick()) * MIDIMain.getPreLength());
+		return (int)(MIDISong.getEvent(track, end).getTick() - MIDISong.getEvent(track, begin).getTick()) * MIDIMain.getPreLength();
 	}
 	
 	//getVolume() returns the volume of the note
 	public byte getVolume()
 	{
-		return MIDISong.getSequence().getTracks()[MIDIMain.getTrackMenu()].get(begin).getMessage().getMessage()[3];
+		//return MIDISong.getMessage(MIDIMain.getTrackMenu(), begin).getMessage()[DATA_VELOCITY];
+		return volume;
 	}
 	
 	//getNumNotes() returns the number of notes counted
@@ -214,10 +173,11 @@ public class Notes
 		return numNotes;
 	}
 	
-	//resetNotes() resets the value of numNotes, it resets the number of notes
+	//resetNotes() resets the value of numNotes, which resets the number of notes
 	public static void resetNotes()
 	{
 		numNotes = 0;
+		track = 0;
 	}
 	
 	//convertToNote(byte c, boolean sharp) returns the note letter assigned to a note value
@@ -301,5 +261,14 @@ public class Notes
 		{
 			return c+"?";
 		}
+	}
+
+	//isMessageStatus(byte status, byte comparison) returns true or false whether the status of the message is a variant of its type
+	public static boolean isMessageStatus(byte mesStatus, byte statusType)
+	{
+		if(mesStatus >= statusType && mesStatus <= statusType+16)
+			return true;
+		else
+			return false;
 	}
 }
