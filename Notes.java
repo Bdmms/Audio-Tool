2dcc4b7ff2bf18df54083606a5ecc13a48c7842b
Bdmms <mmsrannie@gmail.com>
2016-11-21 12:14:43 -0500

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -1,3 +1,4 @@
+import java.awt.Rectangle;
 
 /**
  * Date: October 26, 2016
@@ -10,18 +11,20 @@
 
 public class Notes
 {
-	public final static byte MAX_TONE = 120;	//The maximum note value
-	public final static byte DATA_STATUS = 0;	//value assigned to the message's status
-	public final static byte DATA_TONE = 1;		//value assigned to the tone of the note
-	public final static byte DATA_VELOCITY = 2;	//value assigned to the volume of the note
+	public final static byte MAX_TONE = 120;		//The maximum note value
+	public final static byte DATA_STATUS = 0;		//value assigned to the message's status
+	public final static byte DATA_TONE = 1;			//value assigned to the tone of the note
+	public final static byte DATA_VELOCITY = 2;		//value assigned to the volume of the note
 	
-	private static int numNotes = 0;	//Number of notes processed
-	private static byte track = 0;		//Track being accessed
+	private static int numNotes = 0;				//Number of notes processed
+	private static byte track = 0;					//Track being accessed
+	private static boolean allUnselected = true;	//true if all notes are not selected
 	
 	private int begin = 0;				//start of note in sequence
 	private int end = 0;				//end of note in sequence
 	private byte tone = 60;				//tone of the note
 	private byte volume = 0;			//volume of the note
+	private boolean selected = false;	//whether the note is selected
 	
 	//Constructor method
 	//int s = start of note (in array)
@@ -73,6 +76,50 @@ public class Notes
 		return -1;
 	}
 	
+	//selectContained(Rectangle rect) searches all notes that are contained in the selection box and selects them
+	//Rectangle select = selection box that selects all notes inside of it
+	public static void selectContained(Rectangle select)
+	{
+		for(int i = 0; i < numNotes; i++)
+		{
+			//If notes are visible on screen
+			if(GUI.isNoteVisible(i))
+			{
+				//If any part of the note is inside the box
+				if(select.getX() < MIDISong.getNotes(track)[i].getX() + MIDISong.getNotes(track)[i].getLength() - MIDIMain.getXCoordinate() + GUI.sideBarWidth && select.getX() + select.getWidth() > MIDISong.getNotes(track)[i].getX() - MIDIMain.getXCoordinate() + GUI.sideBarWidth &&
+						select.getY() < MIDISong.getNotes(track)[i].getY() + MIDIMain.getPreHeight() - MIDIMain.getYCoordinate() + GUI.fullAddHeight && select.getY() + select.getHeight() > MIDISong.getNotes(track)[i].getY() - MIDIMain.getYCoordinate() + GUI.fullAddHeight)
+				{
+					MIDISong.getNotes(track)[i].selection(true);
+				}
+			}
+		}
+	}
+	
+	//unSelectAll() changes the selection of every note in the track to false
+	public static void unSelectAll()
+	{
+		//If notes are not already unselected
+		if(allUnselected == false)
+		{
+			for(int i = 0; i < numNotes; i++)
+			{
+				MIDISong.getNotes(track)[i].selection(false);
+			}
+			allUnselected = true;
+		}
+	}
+	
+	//selection(boolean state) sets the selected value of the note
+	//boolean state = state of note selection
+	public void selection(boolean state)
+	{
+		//If a note is selected
+		if(state == true)
+			allUnselected = false;
+		
+		selected = state;
+	}
+	
 	//setTone(byte t) sets the tone value of the note
 	//byte t = new tone
 	public void setTone(byte t)
@@ -87,7 +134,7 @@ public class Notes
 		x = (x - x%MIDIMain.getPreLength()) / MIDIMain.getPreLength();
 			
 		if(x <= 0 || x <= getTick())
-			x = getTick() / MIDIMain.getPreLength() + 1;
+			x = getTick() + 1;
 		
 		MIDISong.getEvent(track, end).setTick(x);
 	}
@@ -148,6 +195,12 @@ public class Notes
 		return (MAX_TONE - tone) * MIDIMain.getPreHeight();
 	}
 	
+	//getX() returns the x location of the note's end
+	public long getEndX()
+	{
+		return getX() + getLength();
+	}
+	
 	//getTone() retuns the tone of the note
 	public byte getTone()
 	{
@@ -165,6 +218,11 @@ public class Notes
 	{
 		//return MIDISong.getMessage(MIDIMain.getTrackMenu(), begin).getMessage()[DATA_VELOCITY];
 		return volume;
+	}
+	
+	public boolean isSelected()
+	{
+		return selected;
 	}
 	
 	//getNumNotes() returns the number of notes counted
@@ -266,6 +324,7 @@ public class Notes
 	//isMessageStatus(byte status, byte comparison) returns true or false whether the status of the message is a variant of its type
 	public static boolean isMessageStatus(byte mesStatus, byte statusType)
 	{
+		//If message is 1 of 16 variants
 		if(mesStatus >= statusType && mesStatus <= statusType+16)
 			return true;
 		else
