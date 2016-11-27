674eab49f50a2a825321f885095ab9b5cb3f93e7
Bdmms <mmsrannie@gmail.com>
2016-11-27 18:56:52 -0500

    Update InfoBar.java

--- InfoBar.java
+++ InfoBar.java
@@ -8,18 +8,18 @@ import javax.swing.JPanel;
 public class InfoBar extends JPanel{
 
 	/**
-	 * Date: October 17, 2016
+	 * Date: Novemeber 21, 2016
 	 * 
 	 * This class is responsible for displaying the information of the song in the track menu
 	 * This information includes the song's tempo, length and time signature
 	 */
 	
 	private static final long serialVersionUID = 2L;
-	private long length = (MIDISong.getSequence().getTickLength()) / 1000000;
-	private String songName = MIDISong.getSequence().toString();
+	//private long length = (MIDISong.getSequence().getTickLength()) / 1000000;
+	//private String songName = MIDISong.getSequence().toString();
 	
 	
-	public void InfoBar (Graphics g)
+	public void paintComponent(Graphics g) 
 	{
 		Graphics2D g2D = (Graphics2D) g;
 		drawInfoBar(g2D);
@@ -28,9 +28,9 @@ public class InfoBar extends JPanel{
 	public void drawInfoBar(Graphics2D g)
 	{
 		g.setColor(Color.LIGHT_GRAY);
-		g.fillRect(15, GUI.screenHeight - 80, (GUI.screenWidth / 2) -15, 60);
+		g.fillRect(0, 0, (GUI.screenWidth / 2) -15, 60);
 		g.setColor(Color.BLACK);
 		g.setStroke(GUI.bold);
-		g.drawRect(15, GUI.screenHeight - 80, (GUI.screenWidth / 2) -15, 60);
+		g.drawRect(0, 0, (GUI.screenWidth / 2) -15, 60);
 	}
 }
