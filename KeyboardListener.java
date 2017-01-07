12fad4a5e1b19f4a867f6868cde4046f48a28781
Bdmms <mmsrannie@gmail.com>
2017-01-07 17:02:30 -0500

    Update KeyboardListener.java

--- KeyboardListener.java
+++ KeyboardListener.java
@@ -2,134 +2,145 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 /**
+ * <b>[Date: October 17, 2016]</b>
+ * <p>
  * This class registers and records keyboard inputs.
- * 
- * Date: October 17, 2016
+ * </p>
  */
-
 public class KeyboardListener implements KeyListener
 {
-	private boolean space, ctrl, delete, enter;
-	private boolean[] letters = new boolean[26];
-	private boolean[] numbers = new boolean[10];
+	private boolean space, ctrl, delete, enter;		//The special keys
+	private boolean[] letters = new boolean[26];	//The letter keys on the keyboard
 
-	/*
-	 * getLetters() returns the boolean value of every letter on the keyboard
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public boolean[] getLetters()}</pre></p> 
+	 * Returns the boolean value of every letter on the keyboard.</p> 
+	 * @return The array of key inputs for letter keys
 	 */
-	public boolean[] getLetters(){
-		
+	public boolean[] getLetters()
+	{
 		return letters;
 	}
 	
-	/*
-	 * setLetter(boolean state, byte letter) sets the boolean value of a specific letter
-	 * boolean state = the boolean value being set to the letter
-	 * byte letter = the index of the letter in the array
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public void setLetter(boolean state, byte letter)}</pre></p> 
+	 * Sets the boolean value of a specific letter.</p> 
+	 * @param state = the boolean value being set to the letter
+	 * @param letter = the index of the letter in the array
 	 */
-	public void setLetter (boolean state, byte letter){
-		
+	public void setLetter(boolean state, byte letter)
+	{
 		letters[letter] = state;
 	}
-	/*
-	 * getNumbers() returns the boolean value of every number on the keyboard
-	 */
-	public boolean [] getNumbers(){
-		
-		return numbers;
-	}
 	
-	/*
-	 * setNumbers(boolean state, byte number) sets the boolean value of a specific number
-	 * boolean state = the boolean value being set to the letter
-	 * byte number = the index of the number in the array
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public boolean getEnter()}</pre></p> 
+	 * Returns the boolean value of the enter key.</p> 
+	 * @return The value of the enter key
 	 */
-	public void setNumbers(boolean state, byte number){
-		
-		numbers[number] = state;
-	}
-	
-	/*
-	 * getEnter() returns the boolean value of the enter key
-	 */
-	public boolean getEnter(){
-		
+	public boolean getEnter()
+	{
 		return enter;
 	}
 	
-	/*
-	 * setEnter(boolean state) sets the boolean value of the enter key
-	 * boolean state = the boolean value of the enter key
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public void setEnter(boolean state)}</pre></p> 
+	 * Sets the boolean value of the enter key.</p> 
+	 * @param state = the boolean value of the enter key
 	 */
-	public void setEnter(boolean state){
-		
+	public void setEnter(boolean state)
+	{
 		enter = state;
 	}
 	
-	/*
-	 * getDelete() returns the boolean value of the delete key
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public boolean getDelete()}</pre></p> 
+	 * Returns the boolean value of the delete key.</p> 
+	 * @return The boolean value of the delete key
 	 */
-	public boolean getDelete(){
-		
+	public boolean getDelete()
+	{
 		return delete;
 	}
 	
-	/*
-	 * setDelete(boolean state) sets the boolean value of the delete key
-	 * boolean state = the boolean value of the delete key
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public void setDelete(boolean state)}</pre></p> 
+	 * Sets the boolean value of the delete key.</p> 
+	 * @param state = the boolean value of the delete key
 	 */
-	public void setDelete(boolean state){
-		
+	public void setDelete(boolean state)
+	{
 		delete = state;
 	}
 	
-	/*
-	 * getBackspace() returns the boolean value of the backsapce key
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public boolean getSpace()}</pre></p> 
+	 * Returns the boolean value of the backspace key.</p> 
+	 * @return The boolean value of the backspace key
 	 */
-	public boolean getSpace(){
-		
+	public boolean getSpace()
+	{
 		return space;
 	}
 	
-	/*
-	 * setBackspapce(boolean state) sets the boolean value of the backspace key
-	 * boolean state = the boolean value of the backsapce key
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public void setSpace(boolean state)}</pre></p> 
+	 * Sets the boolean value of the backspace key.</p> 
+	 * @param state = the boolean value of the backspace key
 	 */
-	public void setSpace(boolean state){
-		
+	public void setSpace(boolean state)
+	{
 		space = state;
 	}
 	
-	/*
-	 * getControl() returns the boolean value of the control key
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public boolean getControl()}</pre></p> 
+	 * Returns the boolean value of the control key.</p> 
+	 * @return The boolean value of the control key
 	 */
-	public boolean getControl(){
-		
+	public boolean getControl()
+	{
 		return ctrl;
 	}
 	
-	/*
-	 * setControl(boolean state) sets the boolean value of the control key
-	 * boolean state = the boolean value of the control key
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public void setControl(boolean state)}</pre></p> 
+	 * Sets the boolean value of the control key.</p> 
+	 * @param state = the boolean value of the control key
 	 */
-	public void setControl(boolean state){
-		
+	public void setControl(boolean state)
+	{
 		ctrl = state;
 	}
 	
-	/*
-	 * mandatory method for the implementation of KeyListener
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public void keyTyped(KeyEvent e)}</pre></p> 
+	 * Mandatory method for the implementation of KeyListener.</p> 
+	 * @param e = the detected event value from the keyboard
 	 */
 	public void keyTyped(KeyEvent e) {
-		
-		
 	}
 
-	/*
-	 * keyPressed(KeyEvent e) checks every key used in the program when it is pressed. When the key is pressed its boolean value is set to true
-	 * KeyEvent e = the detected event value from the keyboard
-	 * 
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public void keyPressed(KeyEvent e)}</pre></p> 
+	 * Checks every key used in the program when it is pressed. When the key is pressed its boolean value is set to true.</p> 
+	 * @param e = the detected event value from the keyboard
 	 */
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent e) 
+	{
+		//Checks every letter
 		for(byte c = 0; c<=25; c++)
 		{
 			//determines if control and the 'c' key are being pressed simultaneously 
@@ -153,28 +164,11 @@ public class KeyboardListener implements KeyListener
 				ctrl = true;
 			}
 			
-			//determines if control and the 'y' key are being pressed simultaneously 
-			if(e.getKeyCode() == KeyEvent.VK_Y && (e.getModifiers() | KeyEvent.CTRL_DOWN_MASK) != 0)
-			{
-				letters [24] = true;
-				ctrl = true;
-			}
-			
 			//sets the pressed letter's boolean value to true in the array of letters
 			else if(e.getKeyChar() - 97 == c)
 			{
 				letters [c] = true;
 				System.out.println((char)(c+97));
-			}
-		}
-		
-		for(byte n = 0; n<=9; n++)
-		{
-			//sets the pressed number's boolean value to true in the array of numbers
-			if(e.getKeyChar() - 48 == n)
-			{
-				numbers [n] = true;
-				System.out.println(n);
 			}
 		}
 		
@@ -196,12 +190,14 @@ public class KeyboardListener implements KeyListener
 		}
 	}
 
-	/*
-	 * keyPressed(KeyEvent e) checks every key used in the program when it is pressed. When the key is released its boolean value is set to false
-	 * KeyEvent e = the detected event value from the keyboard
+	/**
+	 * <blockquote>
+	 * <p><pre>{@code public void keyReleased(KeyEvent e)}</pre></p> 
+	 * Responds to when a key is released.</p> 
+	 * @param e = the detected event value from the keyboard
 	 */
-	public void keyReleased(KeyEvent e) {
-		
+	public void keyReleased(KeyEvent e) 
+	{
 		//determines if the control key is released
 		if(e.getKeyCode() == KeyEvent.VK_CONTROL)
 		{
@@ -231,19 +227,8 @@ public class KeyboardListener implements KeyListener
 			//sets the released letter's boolean value to false in the array of letters
 			if(e.getKeyChar() -97 == c)
 			{
-				letters [c] = false;
-			}
-		}
-		
-		for(byte n = 0; n<=9; n++)
-		{
-			//sets the released number's boolean value to false in the array of numbers
-			if(e.getKeyChar() -48 == n)
-			{
-				numbers [n] = false;
+				letters[c] = false;
 			}
 		}
 	}
-
-	
 }
